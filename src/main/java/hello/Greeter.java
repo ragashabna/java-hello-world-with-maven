5ec6a276c5de50c23388ef78f606f15cf1570c55
esitajaan@gmail.com <esitajaan@gmail.com>
2022-09-27 22:35:36 +0530

    java src file modified

--- src/main/java/hello/Greeter.java
+++ src/main/java/hello/Greeter.java
@@ -2,6 +2,6 @@ package hello;
 
 public class Greeter {
     public String sayHello() {
-        return "devops for webhooks Project for AchiStar Technologies";
+        return "devops Project for AchiStar Technologies";
     }
 }
